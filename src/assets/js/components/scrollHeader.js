3a8e5d32dcaaaa2090bcebcd3905c0010c5b1146
Anton9512 <Anton.mail9512@gmail.com>
2023-03-23 18:07:57 +0300

    footer block

--- src/assets/js/components/scrollHeader.js
+++ src/assets/js/components/scrollHeader.js
@@ -1,7 +1,7 @@
 function scrollHeader() {
   const header = document.getElementById("header");
 
-  if (this.scrollY >= 50) header.classList.add("scroll-header");
+  if (this.scrollY >= 100) header.classList.add("scroll-header");
   else header.classList.remove("scroll-header");
 }
 
